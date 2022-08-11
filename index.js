ddf98a45bec338be87f20bc6665d4c14f54005f5
Fiona Snyder <fiona.snyder@outlook.com>
2022-08-11 17:38:53 -0500

    commit tree cec936eec0c68a8eaf03bed85d528bb6d05a4b76

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books => renderBooks(books))
   
+  /*2nd way to convert the data to json format, then call 2nd function 
+  and passing in the json data as the argument.
+  .then(function(response) {
+    return response.json()
+  }) 
+  .then(function(books) {
+    return renderBooks(books)
+  })*/
 }
 
 function renderBooks(books) {
